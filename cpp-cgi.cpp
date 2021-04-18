486d521f1e7c013f45785d13b24fee778c0aff8f
gor1908 <el_gor@rambler.ru>
2021-04-18 22:55:18 +0000

    fixed cgi-program

--- cpp-cgi.cpp
+++ cpp-cgi.cpp
@@ -1,14 +1,21 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+
 using namespace std;
 
 int main () {
+   //generate random number
+   srand((unsigned) time(0));
    cout << "Content-type:text/html\r\n\r\n";
    cout << "<html>\n";
    cout << "<head>\n";
-   cout << "<title>Hello World - First CGI Program</title>\n";
+   cout << "<title>Hello World - CGI Program</title>\n";
    cout << "</head>\n";
    cout << "<body>\n";
-   cout << "<h2>Hello World! This is my first CGI program</h2>\n";
+   cout << "<h2>Hello ! This is my first CGI program</h2>\n";
+   cout << "<br> It can generate random numbers as: <u> " <<
+	   rand()  << " </u>\n";
    cout << "</body>\n";
    cout << "</html>\n";
    
